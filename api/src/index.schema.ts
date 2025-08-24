87f7e47ec6aed84f44bc57ab4f48b79cca5e4995
hotfusion <korolov.vadim@gmail.com>
2025-08-24 19:15:19 -0400

    .

--- api/src/index.schema.ts
+++ api/src/index.schema.ts
@@ -1,5 +1,6 @@
-export interface IUser {
+export interface IUserAccess {
     email : string
+    password: string
 }
 export interface IBranch {
     name: string
--- api/src/workspace/index.ts
+++ api/src/workspace/index.ts
@@ -1,5 +1,5 @@
-import {Controller,Authorization,REST} from "@hotfusion/ws";
-import type {IUser} from "./schema.ts";
+import {Controller, Authorization, REST, ICTX} from "@hotfusion/ws";
+import {IUserAccess} from "../index.schema";
 
 @Authorization.provider('local')
 export  class Workspace {
@@ -7,8 +7,15 @@ export  class Workspace {
         roles : ['root','dev']
     })
     @REST.get()
-    '_.ws/user'(@REST.schema() settings:IUser){
+    '_.ws/root/create'(@REST.schema() settings : IUserAccess,ctx:ICTX){
+        ctx.getUser().log('hello there')
         return {name:'vadim'}
+    }
+
+    @REST.get()
+    '_.ws/signin'(@REST.schema() settings : IUserAccess,ctx:ICTX){
+        console.log(settings,ctx)
+        return ctx.getUser().createToken('_id:12345678')
     }
     @Controller.on('mounted')
     __mounted(){
